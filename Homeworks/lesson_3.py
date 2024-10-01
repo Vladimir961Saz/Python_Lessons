7cca8ce08a991dfafccf2f285e7da0f0ac09162e
Vladimir Sazonov <vovanisalive4343@gmail.com>
2024-10-01 08:58:54 +0300

    homework

---
+++ Homeworks/lesson_3.py
@@ -0,0 +1,17 @@
+for i in range (1,10):
+    for j in range (1,10):
+        print (i*j, end='\t')
+    print()
+
+a=int(input('Введите верхний предел:'))
+b=0
+s=0
+c=0
+if a>100:
+    print ('Верхний предел должен быть меньше 100!')
+else:
+   while c<a:
+     c+=1
+     b+=1
+     s+=b**3
+     print (int(s))
