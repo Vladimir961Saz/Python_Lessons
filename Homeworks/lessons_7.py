fce60687da81e14a9a9ca2895ceaac56fe9c9aa3
Vladimir Sazonov <vovanisalive4343@gmail.com>
2024-11-05 11:11:16 +0300

    to the git!

--- Homeworks/lessons_7.py
+++ Homeworks/lessons_7.py
@@ -29,3 +29,13 @@ def print_docs(directory):
 print(f'Директории: {", ".join([folder for folder in catalog[1]])}')
 print(f'Файлы: {", ".join([file for file in catalog[2]])}')
 print('-' * 40)
+
+
+def longest_words(file):
+    with open(file, encoding='utf-8') as text:
+        words = text.read().split()
+        max_length = len(max(words, key=len))
+        sought_words = [word for word in words if len(word) == max_length]
+        if len(sought_words) == 1:
+            return sought_words[0]
+        return sought_words
